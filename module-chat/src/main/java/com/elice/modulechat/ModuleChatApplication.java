42b335c595c5e7a319574803adec587101081276
강 대희 <daehee719@gmail.com>
2024-07-25 09:38:30 +0900

    ㅑㅜㅑㅅ먀ㅣ 채ㅡㅡㅑㅅ

---
+++ module-chat/src/main/java/com/elice/modulechat/ModuleChatApplication.java
@@ -0,0 +1,14 @@
+package com.elice.modulechat;
+
+import com.elice.moduleuser.UserComponent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleChatApplication.class, args);
+    }
+
+}
